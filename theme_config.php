a6afe422f3516c6b2874b2f640c6d2ba185aff1b
Jimmi08 <janamicek@gmail.com>
2017-01-04 22:49:56 +0100

    preparing theme files

---
+++ theme_config.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!defined('e107_INIT')) { exit; }
+
+// Dummy Theme Configuration File.
+class theme_config implements e_theme_config
+{
+
+	function config()
+	{
+		// v2.1.4 format.
+
+		$fields = array(
+			 
+ 		);
+
+		return $fields;
+ 
+	}
+
+	function help()
+	{
+	 	return '';
+	}
+}
+
+
+?>
